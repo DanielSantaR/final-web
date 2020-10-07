a9448b1ce58f507945302c0774b6b569ab83cf75
Daniel Santa <daniel.santar@udea.edu.co>
2020-10-06 21:41:18 -0500

    fix routes

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import root, user
+from app.api.endpoints import user
 
 api_router = APIRouter()
 
-api_router.include_router(root.router)
 api_router.include_router(user.router, prefix="/user", tags=["user"])
