b76d1143646387ced91ca86dd26e5a26d6db82cf
Daniel <daniel.torresg@udea.edu.co>
2020-10-21 20:59:53 -0500

    change model employee

--- app/infra/postgres/models/employee.py
+++ app/infra/postgres/models/employee.py
@@ -6,3 +6,4 @@ from app.infra.postgres.models.user import User
 class Employee(User):
     username = fields.CharField(unique=True, max_length=255)
     password = fields.TextField()
+    rol = fields.CharField(max_length=255)
